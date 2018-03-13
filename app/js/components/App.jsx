95dc2cd4670be3acd0f929c8b9f6c08f96baefeb
geofrocker <geofrocker2@gmail.com>
2018-03-13 13:42:29 +0300

    OEUI-29:Initial project setup

---
+++ app/js/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!
+        </h1>
+      </div>
+    );
+  }
+}
