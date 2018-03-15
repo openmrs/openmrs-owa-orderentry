f595712e2ad20abd5b85c999f7690f0e02ef3720
geofrocker <geofrocker2@gmail.com>
2018-03-15 16:15:50 +0300

    ORDER-29:Initial project setup

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -12,7 +12,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!!
+        <h1>Hello, world!
         </h1>
       </div>
     );
