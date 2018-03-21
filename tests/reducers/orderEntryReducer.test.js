6b72b51547e8ee651d3137c3c5bca4b90c5aa8bc
sir3n-sn <sir3n.sn@gmail.com>
2018-03-22 00:33:46 +0300

    OEUI-33: Get order entry configurations from the API.

---
+++ tests/reducers/orderEntryReducer.test.js
@@ -0,0 +1,54 @@
+import deepFreeze from 'deep-freeze';
+import * as actions from '../../app/js/actions/orderEntryActions';
+import orderEntryConfigurations from '../../app/js/reducers/orderEntryReducer';
+
+
+describe('Order Entry Configurations reducer', () => {
+    let initialState;
+    beforeEach(() => {
+        initialState = {
+            configurations: [],
+            isLoading: false,
+            hasError: '',
+        };
+        deepFreeze(initialState);
+    });
+
+    it('should return the initial state', () => {
+        const expectedState = orderEntryConfigurations(initialState, {});
+        expect(initialState).toEqual(expectedState);
+    });
+
+    it('should handle `FETCH_ORDER_CONFIG_SUCCESS`', () => {
+        const configurations = {
+                careSettings: [{ id: 1, display: 'outpatient' }],
+        };
+        const expectedState = {
+            configurations: {
+                careSettings: [{ id: 1, display: 'outpatient' }]
+            },
+            isLoading: false,
+            hasError: ''
+        };
+        const actualState = orderEntryConfigurations(initialState, actions.fetchConfigurationsSuccess(configurations));
+        expect(actualState).toEqual(expectedState);
+    });
+    it('should handle `FETCH_ORDER_CONFIG_LOADING`', () => {
+        const expectedState = {
+            configurations: [],
+            isLoading: true,
+            hasError: ''
+        };
+        const actualState = orderEntryConfigurations(initialState, actions.fetchConfigurationsLoading(true));
+        expect(actualState).toEqual(expectedState);
+    });
+    it('should handle `FETCH_ORDER_CONFIG_FAILURE`', () => {
+        const expectedState = {
+            configurations: [],
+            isLoading: false,
+            hasError: 'Bad request'
+        };
+        const actualState = orderEntryConfigurations(initialState, actions.fetchConfigurationsFailure('Bad request'));
+        expect(actualState).toEqual(expectedState);
+    });
+});
