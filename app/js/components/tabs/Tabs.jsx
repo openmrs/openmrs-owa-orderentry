76e95bf3f53644d954e3b6fc9236ecb1a2848587
flacode <flavianshemerirwe@gmail.com>
2018-03-26 20:27:02 +0300

    OEUI-49: Add tab components that will be used to switch between outpatient and inpatient.

---
+++ app/js/components/tabs/Tabs.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Tabs extends Component {
+    state = {
+      activeTabIndex: this.props.defaultActiveTabIndex,
+    };
+
+    handleTabClick = (tabIndex, tabName) => {
+      this.setState({
+        activeTabIndex: tabIndex,
+      });
+      this.props.careSetting(tabName);
+    }
+
+    passPropsToChildren = () => (
+      React.Children.map(this.props.children, (child, index) => React.cloneElement(child, {
+        onClick: this.handleTabClick,
+        tabIndex: index,
+        isActive: index === this.state.activeTabIndex,
+      })));
+
+    renderActiveTabChildren = () => {
+      const { children } = this.props;
+      const { activeTabIndex } = this.state;
+      if (children[activeTabIndex]) {
+        return children[activeTabIndex].props.children;
+      }
+    }
+
+    render() {
+      return (
+        <div className="ui-tabs">
+          <ul className="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist">
+            { this.passPropsToChildren() }
+          </ul>
+          <div className="ui-tabs-panel ui-widget-content ui-corner-bottom">
+            { this.renderActiveTabChildren() }
+          </div>
+        </div>
+      );
+    }
+}
+Tabs.propTypes = {
+  defaultActiveTabIndex: PropTypes.number,
+  children: PropTypes.node.isRequired,
+  careSetting: PropTypes.func.isRequired,
+};
+
+Tabs.defaultProps = {
+  defaultActiveTabIndex: 0,
+};
+
+export default Tabs;
---
+++ tests/components/tabs/Tab.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Tab from '../../../app/js/components/tabs/Tab';
+
+describe('`Tab` component', () => {
+    it('should render without crashing', () => {
+        const wrapper = shallow(<Tab/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should trigger event when clicked', () => {
+        const handleOnClick = jest.fn();
+        const wrapper = shallow(<Tab onClick={handleOnClick}/>);
+        const tab = wrapper.find('a');
+        expect(handleOnClick.mock.calls.length).toBe(0);
+        tab.simulate('click', {
+            preventDefault: () => {},
+          });
+        expect(handleOnClick.mock.calls.length).toBe(1);
+    });
+});
