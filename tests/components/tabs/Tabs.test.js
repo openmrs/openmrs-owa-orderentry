b693cebd41e2e34116a81db556f1e9a4bfb0c909
Flavia Nshemerirwe <flacode@users.noreply.github.com>
2018-03-27 16:40:14 +0300

    OEUI-49: Add tab components that will be used to switch between outpatient and inpatient. (#8)

---
+++ tests/components/tabs/Tabs.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Tabs from '../../../app/js/components/tabs/Tabs';
+
+describe('`Tabs` component', () => {
+    it('should render without crashing', () => {
+        const wrapper = shallow(<Tabs><p>hello</p></Tabs>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render contents at index 0 by default', () => {
+        const wrapper = mount(<Tabs>
+            <div tabName="tab 1"><p>Content 1</p></div>
+            <div tabName="tab 2"><p>Content 2</p></div>
+            <div tabname="tab 3"><p>Content 3</p></div>
+            </Tabs>);
+        expect(wrapper.find('.ui-tabs-panel').text()).toEqual('Content 1');
+    });
+
+    it('should render only content of active children', () => {
+        const wrapper = mount(<Tabs defaultActiveTabIndex={1}>
+            <div tabName="tab 1"><p>Content 1</p></div>
+            <div tabName="tab 2"><p>Content 2</p></div>
+            <div tabname="tab 3"><p>Content 3</p></div>
+            </Tabs>);
+        expect(wrapper.find('.ui-tabs-panel').text()).toEqual('Content 2');
+    });
+
+    it('should change the content when active index changes', () => {
+        const careSetting = jest.fn();
+        const wrapper = mount(<Tabs defaultActiveTabIndex={1} careSetting={careSetting}>
+            <div tabName="tab 1"><p>Content 1</p></div>
+            <div tabName="tab 2"><p>Content 2</p></div>
+            <div tabname="tab 3"><p>Content 3</p></div>
+            </Tabs>);
+        const children = (wrapper.find('.ui-tabs-nav').children());
+        wrapper.instance().handleTabClick(2, 'tab 3');
+        expect(wrapper.find('.ui-tabs-panel').text()).toEqual('Content 3');
+    });
+});
