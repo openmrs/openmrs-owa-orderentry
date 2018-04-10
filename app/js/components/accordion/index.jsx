b2a77d4a031ca626885c4868edfc1d9447029f96
lawal lanre <larrystonegroup@gmail.com>
2018-04-10 17:24:19 +0100

    OEUI-59: Implement accordion UI/UX for active and past orders

---
+++ app/js/components/accordion/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../../css/accordion.css';
+
+class Accordion extends React.Component {
+  state = {
+    isVisible: this.props.open === undefined ? false : this.props.open,
+  };
+
+  render() {
+    return (
+      <div className={`accordion ${this.props.border ? 'border' : ''}`}>
+        <div className="header">
+          <a
+            role="button"
+            tabIndex={0}
+            onClick={() => {
+              this.setState(() => ({ isVisible: !this.state.isVisible }));
+            }}>
+            <span>
+              {
+                this.state.isVisible ?
+                  <i className="toggle-icon icon-caret-up small rotate180" /> :
+                  <i className="toggle-icon icon-caret-up small rotate90" />
+              }
+            </span>
+            {this.props.title}
+          </a>
+        </div>
+        <div className={`content ${!this.state.isVisible ? 'close' : 'open'}`}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+Accordion.propTypes = {
+  border: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  open: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+};
+
+Accordion.defaultProps = {
+  open: false,
+  border: false,
+};
+
+export default Accordion;
