b62eea27b4df52d5eee2fdb551a283dc5299d30d
lawal lanre <larrystonegroup@gmail.com>
2018-04-16 15:16:18 +0100

    OEUI-66: Fix bug in the drug search

--- app/js/components/searchDrug/View.jsx
+++ app/js/components/searchDrug/View.jsx
@@ -1,53 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const showLoading = loading => (loading && <span> Searching...</span>);
+
+const showError = error => (
+  error && <span className="preformatted-note">{error.data.message}</span>
+);
+
 const View = ({
-  searchDrug, results, loading, error,
+  searchDrug, results, loading, error, focused,
+  onOptionSelected,
+  onChange, value,
 }) => {
   const renderResults = () => (
-    results && results.length > 0 &&
-    <datalist id="drugs">
+    results.length > 0 && focused &&
+    <div className="options-container">
       {results.map(result => (
-        <option value={result.display} key={result.uuid} />
+        <p
+          role="button"
+          className="option"
+          key={result.uuid}
+          onClick={(event) => {
+            onOptionSelected(result.uuid, result.display);
+          }}
+        >{result.display}
+        </p>
+
       ))}
-    </datalist>
+    </div>
   );
 
-  const showLoading = () => (loading ? ' Searching...' : '');
-
-  const showError = () => (error ? error.data.message : '');
+  const noDrugFound = () => (
+    !loading && focused && results.length === 0 &&
+    <span className="preformatted-note">
+      <span className="emoji-sorry" role="img" aria-label="search"> 🔍 </span>
+      No Drug(s) found, please refine your search
+    </span>
+  );
 
   return (
-    <form id="new-order" className="sized-inputs css-form">
-      <p>
-        <span>
-          <label>New order for: </label>
-          <input
-            list="drugs" type="text" id="sel-drug4841input"
-            placeholder="Search for drug...(3 chars min.)" size="40"
-            onChange={event => searchDrug(event.target.value)}
-          />
-          <span>{showLoading()}</span>
-          <span className="preformatted-note">{showError()}</span>
-          {renderResults()}
-        </span>
-      </p>
-    </form>
+    <div className="search-container">
+      <p>New order for: </p>
+      <span>
+        <input
+          value={value}
+          type="text" id="sel-drug4841input"
+          placeholder="Search for drug...(3 chars min.)"
+          onChange={(event) => {
+            onChange(event);
+          }}
+        />
+        {showLoading(loading)}
+        {showError(error)}
+        {noDrugFound()}
+      </span>
+      {renderResults(results)}
+    </div>
   );
 };
-
 
 View.propTypes = {
   results: PropTypes.array,
   searchDrug: PropTypes.func.isRequired,
+  onOptionSelected: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
   error: PropTypes.object,
   loading: PropTypes.bool,
+  focused: PropTypes.bool.isRequired,
 };
 
 View.defaultProps = {
   results: undefined,
   error: null,
   loading: false,
+  value: "",
 };
 
 export default View;
