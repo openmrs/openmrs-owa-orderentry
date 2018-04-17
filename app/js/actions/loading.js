61350f6e19e3a4671d94b91730f8dedbb0f85287
fuchodeveloper <fredrick.mgbeoma@andela.com>
2018-04-17 15:38:43 +0100

    OEUI-67: Create a generic loader

--- app/js/actions/loading.js
+++ app/js/actions/loading.js
@@ -1,5 +1,5 @@
 import { LOADING } from './actionTypes';
 
-const loading = status => ({ type: LOADING, status });
+const loading = (status, action) => ({ type: `LOADING_${action}`, status });
 
 export default loading;
