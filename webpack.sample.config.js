228a8f0986c4cf46c2acc6459f7411f65e3e1a29
mgoodrich <mark.goodrich@live.com>
2018-04-25 11:26:19 -0400

    allow output path to work even if trailing slash is missing

--- webpack.sample.config.js
+++ webpack.sample.config.js
@@ -119,7 +119,7 @@ if (env === 'production') {
 } else if (env === 'deploy') {
 	outputFile = `${outputFile}.js`;
 	vendorOutputFile = "vendor.bundle.js";
-	outputPath = `${config.LOCAL_OWA_FOLDER}${THIS_APP_ID}`;
+	outputPath = `${config.LOCAL_OWA_FOLDER}${config.LOCAL_OWA_FOLDER.slice(-1) != '/' ? '/' : ''}${THIS_APP_ID}`;
 	devtool = 'source-map';
 
 } else if (env === 'dev') {
