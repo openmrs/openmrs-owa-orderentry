297a7b5e6a4536c32a25bdf705b7876ab1369597
Lawal Lanre Emmanuel <larrystonegroup@gmail.com>
2018-05-16 12:41:15 +0100

    OEUI-104 Bug fix for failing app test file (#72)

---
+++ tests/components/searchDrug/index.test.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import _ from 'lodash';
+
+import ConnectedSearchDrug, {
+  SearchDrug
+} from '../../../app/js/components/searchDrug';
+
+
+let props;
+let mountedComponent;
+let clock;
+
+jest.useFakeTimers();
+
+const getComponent = () => {
+  if (!mountedComponent) {
+    mountedComponent = shallow(<SearchDrug {...props} />);
+  }
+  return mountedComponent;
+};
+
+describe('Component: SearchDrug: Container', () => {
+  beforeEach(() => {
+    props = {
+      drugSearch: {
+        drugs: []
+      },
+      searchDrug: jest.fn(),
+      onChange: jest.fn(),
+      onOptionSelected: jest.fn(),
+      selectDrug: jest.fn(),
+      onChange: jest.fn(),
+      onSelectDrug: jest.fn(),
+    };
+    mountedComponent = undefined;
+  });
+
+  it('should render properly', () => {
+    const component = getComponent();
+    expect(component).toMatchSnapshot();
+  });
+
+  describe('Interactions', () => {
+    beforeEach(function () {
+      clock = sinon.useFakeTimers();
+    });
+
+    afterEach(function () {
+      clock.restore();
+    });
+
+    it('should call searchDrug() with debounce feature', () => {
+      const searchDrugSpy = sinon.spy(props, 'searchDrug');
+      sinon.spy(getComponent().instance(), 'searchDrug');
+      getComponent().instance().searchDrug('para');
+      expect(getComponent().instance().searchDrug.calledOnce)
+        .toEqual(true);
+
+      clock.tick(500);
+      expect(searchDrugSpy).toHaveBeenCalled();
+    });
+
+    it('should not call searchDrug()', () => {
+      const searchDrugSpy = sinon.spy(props, 'searchDrug');
+      sinon.spy(getComponent().instance(), 'searchDrug');
+      getComponent().instance().searchDrug('  ');
+
+      clock.tick(500);
+      expect(searchDrugSpy).not.toHaveBeenCalled();
+    });
+
+    it('should call onChange()', () => {
+      const event = {
+        target: {
+          value: "Value"
+        },
+        preventDefault: jest.fn(),
+      };
+      sinon.spy(getComponent().instance(), 'onChange');
+      getComponent().instance().onChange(event);
+      expect(getComponent().instance().onChange.calledOnce)
+        .toEqual(true);
+    });
+
+    it('should call onOptionSelected()', () => {
+      sinon.spy(getComponent().instance(), 'onOptionSelected');
+      getComponent().instance().onOptionSelected('6eecdsd-dfdfdfd', 'Panadol');
+      expect(getComponent().instance().onOptionSelected.calledOnce)
+        .toEqual(true);
+    });
+  });
+
+  describe('Connected SearchDrug component', () => {
+    it('component successfully rendered', () => {
+      const store = mockStore({
+        drugSearchReducer: {
+          drugs: [],
+          loading: false,
+          error: null
+        }
+      });
+      const wrapper = shallow(<ConnectedSearchDrug store={store} />);
+      expect(wrapper.length).toBe(1);
+    });
+  });
+});
