3ae541f195f42a3cca2932fb1843d6aea48918f0
topseySuave <gabrielsuave17@gmail.com>
2018-08-30 13:23:19 +0100

    OEUI-235: Render action buttons for each draft item (#169)

--- app/js/components/Draft.jsx
+++ app/js/components/Draft.jsx
@@ -2,17 +2,54 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import shortid from 'shortid';
+import constants from '../utils/constants';
+import IconButton from './button/IconButton';
 
 export class Draft extends PureComponent {
   renderDraftList = () => {
     const { draftOrders } = this.props;
     return draftOrders.map((order) => {
+      const isPanel = !!order.set;
+      const draftType = !isPanel ? 'single' : 'panel';
       const orderName = order.display || order.drugName;
+      const iconClass = classNames(
+        'scale',
+        {
+          'i-gray': order.urgency === constants.ROUTINE || typeof order.urgency === 'undefined',
+          'i-red': order.urgency === constants.STAT,
+        },
+      );
+
       return (
         <li className="draft-list small-font" key={shortid.generate()}>
+          <span className="order-status">{!order.action ? 'NEW' : order.action}</span>
           <span className="draft-name">{orderName.toLowerCase()}</span>
           <div className="action-btn-wrapper">
-            {/* The action are to be dynamically set */}
+            <span className="action-btn">
+              { order.type !== 'drugorder' ?
+                <IconButton
+                  iconClass={iconClass}
+                  iconTitle="Urgency"
+                  onClick={() => {}}
+                  icon="&#x25B2;"
+                  id="draft-toggle-btn icon-btn-anchor"
+                /> :
+                <IconButton
+                  iconClass="icon-pencil"
+                  iconTitle="EDIT"
+                  onClick={() => {}}
+                  id="draft-toggle-btn icon-btn-anchor"
+                />
+              }
+            </span>
+            <span className="action-btn right">
+              <IconButton
+                iconClass="icon-remove"
+                iconTitle="Discard"
+                id="draft-discard-btn"
+                onClick={() => {}}
+              />
+            </span>
           </div>
         </li>);
     });
