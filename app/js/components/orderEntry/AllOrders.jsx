7c4680087c4a306142d11a1e2882412954623315
Efosa Okpugie <efosaokpugie@gmail.com>
2018-09-09 21:20:56 +0100

    OEUI-242: Implement Pagination for orders list homepage (#174)

--- app/js/components/orderEntry/AllOrders.jsx
+++ app/js/components/orderEntry/AllOrders.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import OrdersTable from './OrdersTable';
 import SortAndFilter from './SortAndFilter';
+import Paginate from './Paginate';
+import fetchOrders from '../../actions/fetchOrders';
 
-const AllOrders = () => (
+export const AllOrders = ({ orders: { totalCount, links }, dispatch, patient: { uuid } }) => (
   <div className="all-orders">
     <div className="orders-breadcrumb">
       <h3>Patient Orders</h3>
@@ -20,7 +24,37 @@ const AllOrders = () => (
         <OrdersTable />
       </tbody>
     </table>
+    <br />
+    <br />
+    <Paginate
+      totalPage={totalCount}
+      nextPageUrl={links ? links[0].uri : ''}
+      prevPageUrl={links && links.length === 2 ? links[1].uri : ''}
+      dispatch={dispatch}
+      fetchNew={fetchOrders}
+      patientId={uuid}
+    />
   </div>
 );
 
-export default AllOrders;
+const mapStateToProps = ({ fetchOrdersReducer: { orders }, patientReducer: { patient } }) => ({
+  orders,
+  patient,
+});
+
+export default connect(mapStateToProps)(AllOrders);
+
+AllOrders.defaultProps = {
+  patient: {
+    uuid: '',
+  },
+};
+
+AllOrders.propTypes = {
+  orders: PropTypes.shape({
+    links: PropTypes.array,
+    totalCount: PropTypes.number,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+  patient: PropTypes.objectOf(PropTypes.string),
+};
--- tests/components/orderentry/AllOrders.test.jsx
+++ tests/components/orderentry/AllOrders.test.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-import AllOrders from '../../../app/js/components/orderEntry/AllOrders';
+import { AllOrders } from '../../../app/js/components/orderEntry/AllOrders';
 
 describe('AllOrders test-suite', () => {
-  const wrapper = shallow(<AllOrders />)
+  const props = {
+    orders: {
+      totalCount: 20,
+    },
+    dispatch: jest.fn(),
+    patient: {
+      uuid: '',
+    },
+  };
+  const wrapper = shallow(<AllOrders {...props} />);
   it('renders component properly', () => {
     expect(wrapper.exists()).toBeTruthy();
-  })
-})
+  });
+
+  it('sets the pagination urls in the Paginate component', () => {
+    wrapper.setProps({
+      ...wrapper.props(),
+      orders: {
+        totalCount: 20,
+        links: [
+          {
+            uri: 'next-page-url'
+          },
+          {
+            uri: 'prev-page-url'
+          }
+        ]
+      },
+    });
+    expect(wrapper.find('Paginate').props().nextPageUrl).toEqual('next-page-url');
+    expect(wrapper.find('Paginate').props().prevPageUrl).toEqual('prev-page-url');
+  });
+});
