68d9fb9d33be51e2801fb118417f2c08c0e9e205
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2018-09-24 15:34:03 +0100

    OEUI-279: Fix Order List page number showing as "NaN" (#191)

--- tests/components/orderentry/Paginate.test.jsx
+++ tests/components/orderentry/Paginate.test.jsx
@@ -10,11 +10,25 @@ const props = {
   fetchNew: jest.fn(),
 }
 
+const props2 = {
+  totalPage: 5,
+  nextPageUrl: 'http://next=page=some=random=page=and=the=length=20',
+  dispatch: jest.fn(),
+  prevPageUrl: '',
+  patientId: '123eeeeeer',
+  fetchNew: jest.fn(),
+}
+
+const wrapper2 = shallow(<Paginate {...props2} />)
 const wrapper = shallow(<Paginate {...props} />)
 
 describe('Pagination component test-suite', () => {
   it('renders properly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should render pagination component when no previousurl is passed', () => {
+    expect(wrapper2).toMatchSnapshot();
   });
 
   it('dispatches action to fetch new data when the next button is clicked', () => {
