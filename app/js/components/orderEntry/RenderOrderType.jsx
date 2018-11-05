a306e581e958bb409af08a8b7824a3dc62b6e634
Desmond <desshub95@gmail.com>
2018-11-05 11:18:32 +0300

    OEUI-297: Fix PropTypes import error OrderEntryPage.jsx file (#213)

--- app/js/components/orderEntry/RenderOrderType.jsx
+++ app/js/components/orderEntry/RenderOrderType.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DrugOrderEntry from '../drugOrderEntry';
 import * as orderTypes from './orderTypes';
 import LabEntryForm from '../labOrderEntry/LabEntryForm';
