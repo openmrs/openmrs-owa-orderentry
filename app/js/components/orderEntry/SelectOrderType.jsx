ae0717c867cfc207fb388aba205b966005e01d95
rowland henshaw <rowland.henshaw@gmail.com>
2019-03-27 15:19:47 +0100

    OEUI-302: modified logic for views

--- app/js/components/orderEntry/SelectOrderType.jsx
+++ app/js/components/orderEntry/SelectOrderType.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as orderTypes from './orderTypes';
 
-const SelectOrderType = ({ switchOrderType, currentOrderType }) => {
+const SelectOrderType = ({ switchOrderType, currentOrderType, page }) => {
   const orderTypesAsObject = Object.values(orderTypes);
+  let displayText = "Add Orders";
+  let clickHandler = () => {};
+  if (page === 'laborders') {
+    displayText = "Add Lab Orders";
+    clickHandler = () => switchOrderType(orderTypesAsObject[1]);
+  }
+
+  if (page === "drugorders") {
+    displayText = "Add Drug Orders";
+    clickHandler = () => switchOrderType(orderTypesAsObject[0]);
+  }
   return (
     <div className="dropdown">
       <div className="add-order-nav">
-        <button>
-          Add Order
+        <button
+          onClick={clickHandler}
+        >
+          {displayText}
         </button>
       </div>
-      <div className="dropdown-content">
+      <div className="dropdown-content" style={{ display: page && "none" }}>
         {orderTypesAsObject.map(orderType => (
           <div
             key={orderType.id}
