2488116698105ffe4fde6a8fd3e97fc8fd615bba
Cosmin Ioan <cioan@pih.org>
2020-01-08 13:20:57 -0500

    UHM-4241, left align text

--- app/js/components/orderEntry/OrderHeader.jsx
+++ app/js/components/orderEntry/OrderHeader.jsx
@@ -15,7 +15,7 @@ const OrderHeader = ({
   const discontinueMsg = intl.formatMessage({ id: "app.orders.discontinue", defaultMessage: "DISCONTINUE" });
   return (
     <Fragment>
-      <th>{order.orderNumber}</th>
+      <th className="th-orderable">{order.orderNumber}</th>
       <th className="th-orderable">{orderable}</th>
       <th>{status}</th>
       <th className="order-actions-btn">
