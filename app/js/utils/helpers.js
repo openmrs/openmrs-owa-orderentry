159e9333cadd0514430a41e3f891e74cd5c57161
Mark Goodrich <mgoodrich@pih.org>
2023-03-21 13:32:21 -0400

    UHM-7070: Lab OWA test result not showing short names

--- app/js/utils/helpers.js
+++ app/js/utils/helpers.js
@@ -4,35 +4,30 @@ export const getConceptShortName = (concept, locale) => {
   const localeShort = locale ? (locale.split('_'))[0] : "en";
 
   if (concept) {
-    if (concept.names){
+    if (concept.names) {
       let foundConcept;
       // first, try to find the preferred SHORT name in the current locale
       foundConcept = concept.names.find(name =>
-          !name.voided && name.conceptNameType === 'SHORT' && name.locale === localeShort && name.localePreferred
-      );
+          !name.voided && name.conceptNameType === 'SHORT' && name.locale === localeShort && name.localePreferred);
       if (!foundConcept) {
         // try to find the SHORT name in the current locale
         foundConcept = concept.names.find(name =>
-            !name.voided && name.conceptNameType === 'SHORT' && name.locale === localeShort
-        );
+            !name.voided && name.conceptNameType === 'SHORT' && name.locale === localeShort);
       }
       if (!foundConcept) {
         // attempt to find the preferred name in the locale
         foundConcept = concept.names.find(name =>
-            !name.voided && name.locale === localeShort && name.localePreferred
-        );
+            !name.voided && name.locale === localeShort && name.localePreferred);
       }
       if (!foundConcept) {
         // attempt to find the preferred short name in English
         foundConcept = concept.names.find(name =>
-            !name.voided && name.conceptNameType === 'SHORT' && name.locale === 'en' && name.localePreferred
-        );
+            !name.voided && name.conceptNameType === 'SHORT' && name.locale === 'en' && name.localePreferred);
       }
       if (!foundConcept) {
         // attempt to find any short name in English
         foundConcept = concept.names.find(name =>
-            !name.voided && name.conceptNameType === 'SHORT' && name.locale === 'en'
-        );
+            !name.voided && name.conceptNameType === 'SHORT' && name.locale === 'en');
       }
       if (!foundConcept) {
         // fall back to display
