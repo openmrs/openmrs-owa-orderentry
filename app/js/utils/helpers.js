624204b6e31f8a8f6e7b90679db9fe517bbaddb8
Mark Goodrich <mgoodrich@pih.org>
2023-03-21 13:14:43 -0400

    UHM-7070: Lab OWA test result not showing short names

--- app/js/utils/helpers.js
+++ app/js/utils/helpers.js
@@ -1,21 +1,41 @@
 /* eslint-disable import/prefer-default-export */
 export const getConceptShortName = (concept, locale) => {
-  let conceptName;
+  let conceptName = null;
   const localeShort = locale ? (locale.split('_'))[0] : "en";
 
   if (concept) {
-    if (concept.names && concept.names.length > 0) {
+    if (concept.names){
       let foundConcept;
-      // first, try to find the SHORT name in the current locale
-      foundConcept = concept.names.find(name => (!name.voided && name.conceptNameType === 'SHORT' && name.locale === localeShort));
-
-      if (typeof foundConcept === 'undefined' || foundConcept == null) {
-        // attempt to find the preferred name in the locale
-        foundConcept = concept.names.find(name => (
-          !name.voided && name.locale === localeShort && name.localePreferred));
+      // first, try to find the preferred SHORT name in the current locale
+      foundConcept = concept.names.find(name =>
+          !name.voided && name.conceptNameType === 'SHORT' && name.locale === localeShort && name.localePreferred
+      );
+      if (!foundConcept) {
+        // try to find the SHORT name in the current locale
+        foundConcept = concept.names.find(name =>
+            !name.voided && name.conceptNameType === 'SHORT' && name.locale === localeShort
+        );
       }
-      if (typeof foundConcept === 'undefined' || foundConcept == null) {
-        // could not find any locale preferred name
+      if (!foundConcept) {
+        // attempt to find the preferred name in the locale
+        foundConcept = concept.names.find(name =>
+            !name.voided && name.locale === localeShort && name.localePreferred
+        );
+      }
+      if (!foundConcept) {
+        // attempt to find the preferred short name in English
+        foundConcept = concept.names.find(name =>
+            !name.voided && name.conceptNameType === 'SHORT' && name.locale === 'en' && name.localePreferred
+        );
+      }
+      if (!foundConcept) {
+        // attempt to find any short name in English
+        foundConcept = concept.names.find(name =>
+            !name.voided && name.conceptNameType === 'SHORT' && name.locale === 'en'
+        );
+      }
+      if (!foundConcept) {
+        // fall back to display
         conceptName = concept.display;
       } else {
         conceptName = foundConcept.name;
